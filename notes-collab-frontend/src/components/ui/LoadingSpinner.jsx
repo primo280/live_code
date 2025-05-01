4e7de972cb37b2f249092e5cfcc73352a8d9d6dd
primo280 <dohaprimael06@gmail.com>
2025-05-02 01:32:45 +0200

    first release

--- notes-collab-frontend/src/components/ui/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="loading-spinner">
-      <div className="spinner"></div>
-      <p>Chargement en cours...</p>
-    </div>
-  )
-}
